624dadf2e5df0006b58dc535cfb124e906bfadb3
Saikrishna-MSFT <60575415+Saikrishna-MSFT@users.noreply.github.com>
2021-01-15 12:10:59 +0530

    Disabled the welcome card functionality

--- Source/Microsoft.Teams.Apps.CompanyCommunicator/Bot/UserTeamsActivityHandler.cs
+++ Source/Microsoft.Teams.Apps.CompanyCommunicator/Bot/UserTeamsActivityHandler.cs
@@ -88,89 +88,88 @@ namespace Microsoft.Teams.Apps.CompanyCommunicator.Bot
             }
         }
 
-        /// <summary>
-        /// Gets called when when members other than the bot join the conversation.
-        /// </summary>
-        /// <param name="membersAdded">A list of all the members added to the conversation.</param>
-        /// <param name="turnContext">A strongly-typed context object for this turn.</param>
-        /// <param name="cancellationToken"> A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
-        /// <returns>A task that represents the work queued to execute.</returns>
-        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
-        {
-            this.logger.LogWarning("Inside OnMembersAddedAsync()");
-            try
-            {
-                var credentials = new MicrosoftAppCredentials(this.configuration["AuthorAppId"], this.configuration["AuthorAppPassword"]);
-                ConversationReference conversationReference = null;
-                foreach (var member in membersAdded)
-                {
-                    if (member.Id != turnContext.Activity.Recipient.Id)
-                    {
-                        var proactiveMessage = MessageFactory.Attachment(this.cardHelper.GetWelcomeCard());
-                        proactiveMessage.TeamsNotifyUser();
-                        var conversationParameters = new ConversationParameters
-                        {
-                            IsGroup = false,
-                            Bot = turnContext.Activity.Recipient,
-                            Members = new ChannelAccount[] { member },
-                            TenantId = turnContext.Activity.Conversation.TenantId,
-                        };
-                        await ((BotFrameworkAdapter)turnContext.Adapter).CreateConversationAsync(
-                            turnContext.Activity.ChannelId,
-                            turnContext.Activity.ServiceUrl,
-                            credentials,
-                            conversationParameters,
-                            async (t1, c1) =>
-                            {
-                                conversationReference = t1.Activity.GetConversationReference();
-                                await ((BotFrameworkAdapter)turnContext.Adapter).ContinueConversationAsync(
-                                    this.configuration["AuthorAppId"],
-                                    conversationReference,
-                                    async (t2, c2) =>
-                                    {
-                                        await t2.SendActivityAsync(proactiveMessage, c2);
-                                    },
-                                    cancellationToken);
-                            },
-                            cancellationToken);
-                    }
-                    else
-                    {
-                        // var connectorClient = new ConnectorClient(new Uri(turnContext.Activity.ServiceUrl), this.configuration["AuthorAppId"], this.configuration["AuthorAppPassword"]);
-                        // var message = turnContext.Activity;
-                        // var channelData = message.GetChannelData<TeamsChannelData>();
-                        // var teamorConversationId = channelData.Team != null ? channelData.Team.Id : message.Conversation.Id;
-                        // var members = await connectorClient.Conversations.GetConversationMembersAsync(teamorConversationId);
-                        // foreach (var mem in members)
-                        // {
-                        //    var card = this.cardHelper.GetWelcomeCard();
-                        //    var replyMessage = Activity.CreateMessageActivity();
-                        //    var parameters = new ConversationParameters
-                        //    {
-                        //        Members = new ChannelAccount[] { new ChannelAccount(mem.Id) },
-                        //        ChannelData = new TeamsChannelData
-                        //        {
-                        //            Tenant = channelData.Tenant,
-                        //            Notification = new NotificationInfo() { Alert = true },
-                        //        },
-                        //    };
+        ///// <summary>
+        ///// Gets called when when members other than the bot join the conversation.
+        ///// </summary>
+        ///// <param name="membersAdded">A list of all the members added to the conversation.</param>
+        ///// <param name="turnContext">A strongly-typed context object for this turn.</param>
+        ///// <param name="cancellationToken"> A cancellation token that can be used by other objects or threads to receive notice of cancellation.</param>
+        ///// <returns>A task that represents the work queued to execute.</returns>
+        // protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        // {
+        //    this.logger.LogInformation("Inside OnMembersAddedAsync()");
+        //    try
+        //    {
+        //        var credentials = new MicrosoftAppCredentials(this.configuration["AuthorAppId"], this.configuration["AuthorAppPassword"]);
+        //        ConversationReference conversationReference = null;
+        //        foreach (var member in membersAdded)
+        //        {
+        //            if (member.Id != turnContext.Activity.Recipient.Id)
+        //            {
+        //                var proactiveMessage = MessageFactory.Attachment(this.cardHelper.GetWelcomeCard());
+        //                proactiveMessage.TeamsNotifyUser();
+        //                var conversationParameters = new ConversationParameters
+        //                {
+        //                    IsGroup = false,
+        //                    Bot = turnContext.Activity.Recipient,
+        //                    Members = new ChannelAccount[] { member },
+        //                    TenantId = turnContext.Activity.Conversation.TenantId,
+        //                };
+        //                await ((BotFrameworkAdapter)turnContext.Adapter).CreateConversationAsync(
+        //                    turnContext.Activity.ChannelId,
+        //                    turnContext.Activity.ServiceUrl,
+        //                    credentials,
+        //                    conversationParameters,
+        //                    async (t1, c1) =>
+        //                    {
+        //                        conversationReference = t1.Activity.GetConversationReference();
+        //                        await ((BotFrameworkAdapter)turnContext.Adapter).ContinueConversationAsync(
+        //                            this.configuration["AuthorAppId"],
+        //                            conversationReference,
+        //                            async (t2, c2) =>
+        //                            {
+        //                                await t2.SendActivityAsync(proactiveMessage, c2);
+        //                            },
+        //                            cancellationToken);
+        //                    },
+        //                    cancellationToken);
+        //            }
+        //            else
+        //            {
+        //                var connectorClient = new ConnectorClient(new Uri(turnContext.Activity.ServiceUrl), this.configuration["AuthorAppId"], this.configuration["AuthorAppPassword"]);
+        //                var message = turnContext.Activity;
+        //                var channelData = message.GetChannelData<TeamsChannelData>();
+        //                var teamorConversationId = channelData.Team != null ? channelData.Team.Id : message.Conversation.Id;
+        //                var members = await connectorClient.Conversations.GetConversationMembersAsync(teamorConversationId);
+        //                foreach (var mem in members)
+        //                {
+        //                    var card = this.cardHelper.GetWelcomeCard();
+        //                    var replyMessage = Activity.CreateMessageActivity();
+        //                    var parameters = new ConversationParameters
+        //                    {
+        //                        Members = new ChannelAccount[] { new ChannelAccount(mem.Id) },
+        //                        ChannelData = new TeamsChannelData
+        //                        {
+        //                            Tenant = channelData.Tenant,
+        //                            Notification = new NotificationInfo() { Alert = true },
+        //                        },
+        //                    };
 
-                        // var conversationResource = await connectorClient.Conversations.CreateConversationAsync(parameters);
-                        //    replyMessage.ChannelData = new TeamsChannelData() { Notification = new NotificationInfo(true) };
-                        //    replyMessage.Conversation = new ConversationAccount(id: conversationResource.Id.ToString());
-                        //    replyMessage.TextFormat = TextFormatTypes.Xml;
-                        //    replyMessage.Attachments.Add(card);
-                        //    await connectorClient.Conversations.SendToConversationAsync((Activity)replyMessage);
-                        // }
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                this.logger.LogError("Exception OnMembersAddedAsync() : " + ex.ToString());
-            }
-        }
-
+        // var conversationResource = await connectorClient.Conversations.CreateConversationAsync(parameters);
+        //                    replyMessage.ChannelData = new TeamsChannelData() { Notification = new NotificationInfo(true) };
+        //                    replyMessage.Conversation = new ConversationAccount(id: conversationResource.Id.ToString());
+        //                    replyMessage.TextFormat = TextFormatTypes.Xml;
+        //                    replyMessage.Attachments.Add(card);
+        //                    await connectorClient.Conversations.SendToConversationAsync((Activity)replyMessage);
+        //                }
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        this.logger.LogError("Exception OnMembersAddedAsync() : " + ex.ToString());
+        //    }
+        // }
         private bool IsTeamInformationUpdated(IConversationUpdateActivity activity)
         {
             if (activity == null)
